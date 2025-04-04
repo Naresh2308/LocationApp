c797a9a68389fdb8ebd4cab78ba7bdfe9a5d3d45
Naresh2308 <114303154+Naresh2308@users.noreply.github.com>
2025-04-04 20:43:27 +0530

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import Tabs from "expo-router";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Header } from "react-native/Libraries/NewAppScreen";
+import LandingPage from "./LandingPage";
+
+export type RootStackParamList = {
+  LandingPage: undefined;
+  PhoneNumberScreen: undefined;
+  PANInputScreen: undefined;
+  OTPScreen: undefined;
+  VerifiedScreen: undefined;
+  Dashboard: undefined;
+  MutualFundsPage: undefined;
+  StockPortfolio: undefined;
+  DetailedAnalysis: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <Stack.Navigator initialRouteName="LandingPage">
+      <Stack.Screen name="LandingPage" component={LandingPage} options={{headerShown:false}} />
+      {/* <Stack.Screen name="PhoneNumberScreen" component={PhoneNumberScreen} options={{headerShown:false}} />
+      <Stack.Screen name="PANInputScreen" component={PANInputScreen} options={{headerShown:false}}/>
+      <Stack.Screen name="OTPScreen" component={OTPScreen} options={{headerShown:false}} />
+      <Stack.Screen name="VerifiedScreen" component={VerifiedScreen} options={{headerShown:false}} />
+      <Stack.Screen name="Dashboard" component={Dashboard}  options={{headerShown:false}}/>
+      <Stack.Screen name="MutualFundsPage" component={MutualFundsPage} options={{headerShown:false}}/>
+      <Stack.Screen name="StockPortfolio" component={StockPortfolio} options={{headerShown:false}}/>
+      <Stack.Screen name="DetailedAnalysis" component={DetailedAnalysis} options={{headerShown:false}}/> */}
+    </Stack.Navigator>
+  );
+}
